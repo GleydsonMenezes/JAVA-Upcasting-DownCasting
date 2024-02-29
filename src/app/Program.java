9e40fb20489d5adb7b85bad80fd2fc6f01a0357d
Gleydson Menezes <gsmenezes996@gmail.com>
2024-02-29 16:35:03 -0300

    Override - done

--- src/app/Program.java
+++ src/app/Program.java
@@ -8,7 +8,7 @@ public class Program {
 
 	public static void main(String[] args) {
 		
-		Account acc = new Account(1001, "Alex", 0.0);
+	/*	Account acc = new Account(1001, "Alex", 0.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
 		
@@ -31,8 +31,19 @@ public class Program {
 		 if (acc3 instanceof SavingsAccount) {
 			 SavingsAccount acc5 = (SavingsAccount)acc3;
 			 acc5.updateBalance();
-			 System.out.println("Update!");
-		 }
+			 System.out.println("Update!"); 
+		 } */
+		
+		
+		Account acc1 = new Account(1001, "Alex", 1000.0);
+		acc1.withdraw(200.0);
+		System.out.println(acc1.getHolder() + " Balance: " + acc1.getBalance());
+		
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getHolder() + " Balance: " + acc2.getBalance());
+		
+		
 		
 	}
 
--- src/entities/SavingsAccount.java
+++ src/entities/SavingsAccount.java
@@ -25,5 +25,9 @@ public class SavingsAccount extends Account {
 		balance+= balance * interestRate;
 	}
 	
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
 	
 }
